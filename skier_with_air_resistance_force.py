0922f01c40578755d363919c6e33c48237437a06
Marta Rylko <rylko.marta@gmail.com>
2012-04-06 22:17:11 +0200

    imporved the model, now air drag is proportional to square of velocity starting on a constant B.

--- skier_with_air_resistance_force.py
+++ skier_with_air_resistance_force.py
@@ -5,9 +5,7 @@ from scipy.integrate import odeint
 
 '''
 This module defines a first version of the skier-movement model. The model assumes
-constant friction and air resistance force that depends on velocity
-
-//TODO improve the comment
+constant friction and air drag force that depends on velocity
 '''
 
 
@@ -15,36 +13,42 @@ def _vectorfield(w, t, params):
     '''
     Right hand side of the differential equation.
     
-    d2x/dt2 = g*sin(alfa) - mi*g*cos(alfa) - k* (dx/dt)^2 /m 
+    d2x/dt2 = g*sin(alfa) - mi*g*cos(alfa) - k1 * (dx/dt) / m - k2* (dx/dt)^2 /m 
     
      '''
     x, v = w
-    alfa, mi, k, m  = params
+    alfa, mi, k1, k2, m  = params
     
     f = [v,                                     # dx/dt
-         g*sin(alfa)-mi*g*cos(alfa)- k/m*v*v]    # dv/dt
+         g*sin(alfa)-mi*g*cos(alfa)- k1/m*v -k2/m*v*v]    # dv/dt
     return f
 
-def solver(t, params, w=[0,0]):
+def solver(t, x0, v0, alfa, mi, k1, k2, m, B=4):
     '''
     Solves the move equation. Move happens on an inclined plane with 
     rules of uniformly accelerated motion.
     Air resistance force is dependent with velocity, friction is constant.
     Arguments:
-        w: vector of initial conditions (for time = 0) w = [x, v]
-            x: initial position (one dimension vector???)
-            v: initial velocity in m/s
+        x0: initial position 
+        v0: initial velocity in m/s
         t: time - list of discrete time samples to be considered e.g. np.linspace(0, 5, 21)
-        params: vector of the parameters params = (alfa, mi, k, m):
-            alfa: slope degree (degree between  B and C)
-               |\     
-               |  \
-            A  |    \  C
-               | alfa \
-               |________\
-                   B   
-            mi: coefficient of friction       
-            k: resistance factor
-            m: skier mass in kg
+        alfa: slope degree (degree between  B and C)
+           |\     
+           |  \
+        A  |    \  C
+           | alfa \
+           |________\
+               B   
+        mi: coefficient of friction       
+        k1: air drag factor
+        k2: air drag 
+        m : skier mass in kg
+        B : boundary value (in m/s) from with air drag becomes 
+                proportional to the square of the velocity.
     '''
-    return odeint(_vectorfield, w, t, args=(params,) ) 
+    if v0 <= B:
+        k2 = 0
+    else:
+        k1 = 0
+    params = [alfa, mi, k1, k2, m]
+    return odeint(_vectorfield, [x0, v0], t, args=(params,) ) 
