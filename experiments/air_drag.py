69deec34bebbc9ec4dbd4404a1f10f097b2dbd44
Marta Rylko <rylko.marta@gmail.com>
2012-04-06 23:05:00 +0200

    adjusted experiments to new simulation

--- experiments/air_drag.py
+++ experiments/air_drag.py
@@ -1,11 +1,38 @@
-from simulation import SkierSimulation,skier_with_air_resistance_force
+from simulation import SkierSimulation, Skier, skier_with_air_resistance_force
 from math import pi
+import pylab
 
 '''
 This experiment checks the behaviour of the skiers with different air drag
 '''
 
-sim = SkierSimulation()
-sim.add_racer(0.1, pi/3, 0.3, 70, skier_with_air_resistance_force.solver)
-sim.add_racer(0.1, pi/3, 0.1, 70, skier_with_air_resistance_force.solver)
+mi = 0.05 #waxed skis - typical value
+alfa= pi/12 #15` degrees
+roh = 1.32 #kg*(m^(-3))
+
+m = 60 #kg
+C = 0.6  #drag coficiant, typical values (0.4 - 1)
+
+A_A = 0.2 # m^2- medium position between upright and tucked
+k2_A = 0.5 * C * roh * A_A
+
+A_B = 0.16 # m^2- tucked position
+k2_B = 0.5 * C * roh * A_B
+
+k1 = 0.05 #imaginary value
+x0 = 0
+v0 = 0
+sim = SkierSimulation(solver=skier_with_air_resistance_force.solver, time_zoom=100)
+s_A = Skier(mi, alfa, k1, k2_A, m, x0, v0)
+s_B = Skier(mi, alfa, k1, k2_B, m, x0, v0)
+sim.add_racer(s_A)
+sim.add_racer(s_B)
 sim.run()
+print 'Time difference between A and B is %f seconds' %(s_A.result - s_B.result)
+pylab.plot(sim.timeline,s_A.positions, 
+           sim.timeline,s_B.positions,)
+pylab.legend(('medium position', 'tucked position'), loc='lower right')
+pylab.xlabel("time in seconds")
+pylab.ylabel("distance in meters")
+pylab.grid(True)
+pylab.show()
