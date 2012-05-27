d4d3dce426d71372599ea74242c89bb3be59db57
Anna Skiba <annam.skiba@gmail.com>
2012-05-28 00:26:52 +0200

    Spelling changes and main updated.

--- controller.py
+++ controller.py
@@ -1,17 +1,17 @@
 '''
-    This module define the structure of controllers that decife what the next
+    This module defines the structure of controllers that decide what the next
     kappa should be.
-    There can be a hierarcy of controllers: for example a master controller, that
+    There can be a hierarchy of controllers: for example a master controller that
     decides which lower level controller should be used in a given moment of time.
-    For example a master controller decides then in a given moment, a racer should 
-    change his direction, so he changes the racer's contoler to 'changing direction' controller
-    and then when he decides that changing direction ended, he reintrodues 'regular' controller to handle racer movement
+    For example a master controller decides that in a given moment a racer should 
+    change his direction, so he changes the racer's controller to 'changing direction' controller
+    and then when he decides that changing direction ended, he reintroduces 'regular' controller to handle racer movement
 '''
 
 class Controller:
     '''
     Skeleton for controllers.
-    components that uses the contoller should be interested only in control method.
+    components that uses the controller should be interested only in control method.
     Implementation of the controller should mainly focus on all: desired_curvature, possible_curvature,
     and also probably 'control'
     '''    
@@ -25,9 +25,9 @@ class Controller:
     
     def possible_curvature(self, desired_curvature, racer, *args, **kwargs):
         '''
-        return a curvature that is conputed based on the result
-        of desired_curvature and confronting this to phisics
-        conditions and limitatons that they causes.
+        returns a curvature that is computed based on the result
+        of desired_curvature and confronting this to physics
+        conditions and limitations that they causes.
         '''
 
         # TODO this is a method stub
@@ -43,6 +43,3 @@ class Controller:
         possible_curvature = self.possible_curvature(desired_curvature, racer, *args, **kwargs)
         return possible_curvature
 
-
-            
-        
--- experiments/sample_controllers.py
+++ experiments/sample_controllers.py
@@ -16,8 +16,8 @@ class DirectionDecisionController:
     def __init__(self,  movement_controller, change_controller, x_change=10):
         self.movement_controller = movement_controller
         self.change_controller = change_controller
-        # what is the x when we shoud change direction
-        self.x_change= 10
+        # what is the x when we should change direction
+        self.x_change= x_change
         self.recently_passed_change = False
     
     def control(self, racer, *args, **kwargs):
@@ -25,7 +25,7 @@ class DirectionDecisionController:
         if x > self.x_change-0.1 and x <  self.x_change+0.1:
             if not self.recently_passed_change:
                 self.recently_passed_change = True
-                # change contorller should least only for one kwant of time - change the sign
+                # change controller should least only for one quantum of time - change the sign
                 return self.change_controller.control(racer, *args, **kwargs)
         else:
             self.recently_passed_change = False
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     from math import pi
     import skier_with_air_resistance_force
     from simulation import SkierSimulation
+    from hop_turn_controller import HopTurningController,StraightGoingController,RightTurnController,LeftTurnController
     mi = 0.05 #waxed skis - typical value
     
     alfa = pi/6
@@ -54,9 +55,15 @@ if __name__ == '__main__':
     x0 = vector(0,0)
     v0 = vector(0,19)   #'''sqrt(2000)'''
     racer = Skier(mi, alfa, k1, k2, m, x0, v0, kappa)
+    racer2 = Skier(mi, alfa, k1, k2, m, x0, v0, kappa)
     racer.controller = DirectionDecisionController(movement_controller=SampleCurvatureController(),
-                                                   change_controller=SampleChangeController())
-    sim = SkierSimulation(distance=100, interval=0.001, solver=skier_with_air_resistance_force.solver, time_zoom=100)
+                                                  change_controller=SampleChangeController())
+    racer2.controller = HopTurningController(right_turning_controller=RightTurnController(kappa),
+                                    left_turning_controller=LeftTurnController(kappa),
+                                    straight_controller=StraightGoingController(), 
+                                    kappa=kappa, boundary_val=0.2)
+    sim = SkierSimulation(distance=200, interval=0.001, solver=skier_with_air_resistance_force.solver, time_zoom=100)
     sim.add_racer(racer)
+    sim.add_racer(racer2)
     sim.run()
     
